5ae5d93be97869ade9795438b86be0dc92effd63
LABCAT <shane.watters.nz@gmail.com>
2019-04-03 08:29:45 +1300

    Further development on the functionality required to update an airtable record when the user record in WordPress changes.

--- includes/class-wpac-user-profiles.php
+++ includes/class-wpac-user-profiles.php
@@ -32,16 +32,15 @@ if ( ! class_exists( 'WPAC_User_Profiles', false ) ) {
 
         public function after_profile_update( $user_id, $old_user_data ){
             $airtable_id = get_user_meta( $user_id, '_airtable_id', true );
-            $user_email = $old_user_data->user_email;
-            $user_login = $old_user_data->user_login;
-            $record = $this->update_airtable_record( $airtable_id, $user_email, $user_login );
+            $user_data = get_userdata( $user_id );
+            $record = $this->update_airtable_record( $airtable_id, $user_data );
         }
 
         public function profile_update( $user_id ){
 
         }
 
-        public function update_airtable_record( $airtable_id, $user_email, $user_login ){
+        public function update_airtable_record( $airtable_id, $user_data ){
             $record = new ArrayObject();
             $query = new AirpressQuery( "Employer", 0 );
             $employers = new AirpressCollection( $query );
@@ -53,37 +52,74 @@ if ( ! class_exists( 'WPAC_User_Profiles', false ) ) {
 
             // no record yet - user likely doesn't have an airtable id stored in usermeta
             // lookup via email
-            if( ! sizeof( $record ) ){
-                $record = $employers->lookup( $this->get_mapped_fields( 'user_email' ) , $user_email );
+            if( ! $record || ! sizeof( $record ) ){
+
+                $airtable_field = $this->get_mapped_fields( 'user_email' );
+
+                if( ! empty( $airtable_field ) ){
+                    $record = $employers->lookup( $airtable_field , $user_data->user_email );
+                }
             }
 
             // still no record - user may have changed their email so check by user login
-            if( ! sizeof( $record ) ){
-                $record = $employers->lookup( $this->get_mapped_fields( 'user_login' ), $user_login );
+            if( ! $record || ! sizeof( $record ) ){
+
+                $airtable_field = $this->get_mapped_fields( 'user_login' );
+
+                if( ! empty( $airtable_field ) ){
+                    $record = $employers->lookup( $airtable_field , $user_data->user_login );
+                }
+            }
+
+            $user_fields = [];
+            $mapped_fields = $this->get_mapped_fields();
+
+            foreach( $mapped_fields as $wp_field => $airtable_field  ){
+                if( isset( $user_data->$wp_field ) ){
+                    $user_fields[ $airtable_field ] = $user_data->$wp_field;
+                }
+                else {
+                    $user_fields[ $airtable_field ] = get_user_meta( $user_data->ID, $wp_field, true );
+                }
             }
 
             // still no record - now we need to create one
-            if( ! sizeof( $record ) ){
-
+            if( ! $record || ! sizeof( $record ) ){
+                $new_user[ 'fields' ] = $user_fields;
+                $record = new AirpressRecord( $new_user, $employers );
             }
 
-            $mapped_fields = $this->get_mapped_fields('user_login');
+            // test the record exists just in case it still hasn't been found or created
+            if( $record && sizeof( $record ) ){
+                $record->update( $user_fields );
 
-            $user_fields = [
-                'Website first_name' => 'Bob',
-                'Website last_name' => 'Burgerman'
-            ];
+                if( ! $airtable_id ){
+                    update_user_meta( $user_id, '_airtable_id', $record->record_id() );
+                }
+            }
 
-            $record->update( $user_fields );
         }
 
 
         public function get_mapped_fields( $single_field = '' ){
+            $mapped_fields = [];
             $current_users_mapping = get_option( 'users_table_mapping' );
+            if( is_array( $current_users_mapping ) ){
+                $mapped_fields = array_merge( $mapped_fields, $current_users_mapping );
+            }
+
             $current_usermeta_mapping = get_option( 'usermeta_table_mapping' );
-            $mapped_fields = array_merge( $current_users_mapping, $current_usermeta_mapping );
+            if( is_array( $current_usermeta_mapping ) ){
+                $mapped_fields = array_merge( $mapped_fields, $current_usermeta_mapping );
+            }
+
             if( $single_field ){
-                return $mapped_fields[ $single_field ];
+                if( isset( $mapped_fields[ $single_field ] ) ){
+                    return $mapped_fields[ $single_field ];
+                }
+                else {
+                    return [];
+                }
             }
             return $mapped_fields;
         }
